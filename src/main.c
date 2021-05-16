f1c5bca274418109a7d4509835863ad9ad422ab7
vasvaska <vasvaska@gmail.com>
2021-05-16 19:09:33 +0200

    Assignment point 6) a, b and c.

--- src/main.c
+++ src/main.c
@@ -4,8 +4,11 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
-
+#include <data.h>
+#include <avr/pgmspace.h>
 #define BAUDRATE 115200
+
+
 
 /* Format
  Byte 1: Sync-Value (0xA5, constant)
@@ -25,13 +28,18 @@
  Byte 15: Channel 5 value high byte
  Byte 16: Channel 5 value low byte
  Byte 17: Value of PIND (GPIO input pin states of Port D)
- *
- */
 
+ */
+volatile uint8_t sampleRate =100;//in Hz
 volatile uint8_t package[17] = { 0xA5, 0x5A, 0x02, 0 };
 volatile uint8_t package_counter = 1;
+volatile uint16_t simulation_counter = 0;
+volatile uint8_t simulation_channel =5;
 volatile uint8_t channel = 0;
 
+uint16_t read_int(uint16_t adress){
+	return pgm_read_word(adress);
+}
 void init_UART(uint32_t baud) {
 
 	uint16_t br = F_CPU / 8 / baud - 1;		//Set BitRate
@@ -39,7 +47,7 @@ void init_UART(uint32_t baud) {
 	UBRR0L = (uint8_t) (br & 0xff);			//Load Low BR byte
 
 	UCSR0A |= (1 << U2X0);					//Enable 2x speed
-	UCSR0B |= (1 << TXEN0);					//Enable Transmit
+	UCSR0B |= (1 << TXEN0)+(1<<RXEN0);//Enable Transmit
 	UCSR0B |= (1 << RXCIE0); 				//Enable UART receive interrupts
 }
 
@@ -57,9 +65,11 @@ ISR (USART_RX_vect) { 			//UART Read interrupt for Debug purposes
 
 	char buff = UDR0;
 	PORTB ^= (1 << 5);				//Read UART input
-	if ((buff == 'a') || (buff == 'A')) {
-		//ADCSRA |= (1 << ADSC);		//Start a single conversion
-		PORTB ^= (1 << 5);
+	if ((buff == 'a') || (buff == 'A')) {//Start a single conversion
+		ADCSRA |= (1 << ADSC);
+	}
+	if ((buff == 'd') || (buff == 'D')) {//start timer
+		TCCR1B ^= (1 << 1);
 	}
 }
 
@@ -67,9 +77,26 @@ ISR (ADC_vect) {		//ADC complete interrupt
 
 	if (channel == 6) {
 		channel = 0;
-		package[16] = PIND;				//write PIND to byte 17
+		//write last pack from
+
+		package[16] = PIND;		//write PIND to byte 17
 		UCSR0B |= (1 << UDRIE0);		//Enable USART_UDRE interrupts
-	} else {
+	} else if(channel==simulation_channel){
+
+		int adc_val = ADCW;//read adc
+		float scale=adc_val/1024.*5;//calculate %boost based on ADC value
+		//read prog mem and write it to package[0 and 1]
+		int value =pgm_read_word(&(data[simulation_counter));
+		simulation_counter+=(sampleRate/100);//increase counter based on the counterrrate
+		if(simulation_counter>=15000)simulation_counter-=15000;
+		value*=scale+1;
+		package[5 + (2 * channel)]=(char)value&0xFF;
+		package[4 + (2 * channel)]=(char)(value>>8);
+		ADMUX &= ~(0x07);
+		ADMUX |= ++channel;				//Select next channel
+		ADCSRA |= (1 << ADSC); 			//Initiate next channel ADC-on
+
+	}else{
 		package[5 + (2 * channel)] = ADCL;
 		package[4 + (2 * channel)] = ADCH;
 		ADMUX &= ~(0x07);
